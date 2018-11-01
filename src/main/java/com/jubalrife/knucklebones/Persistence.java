28f909348fbab4e76094f4e592cd35a5fbdeb242
william.lowery@rocky.edu <william.lowery@rocky.edu>
2018-10-31 21:54:14 -0600

    Add Lexer, Parser for Colon Parameter Language to handle parameters in queries. Wired Parameterized Query into Persistence's find. WWL

--- src/main/java/com/jubalrife/knucklebones/Persistence.java
+++ src/main/java/com/jubalrife/knucklebones/Persistence.java
@@ -2,6 +2,7 @@ package com.jubalrife.knucklebones;
 
 import java.lang.reflect.Method;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
@@ -9,14 +10,26 @@ import java.util.*;
 public class Persistence {
 
     private Connection connection;
+    private HashMap<String, Object> parameters = new HashMap<>();
 
     public Persistence(Connection connection) {
         this.connection = connection;
     }
 
     public <DAO> List<DAO> find(String query, Class<DAO> type) throws SQLException {
-        try (ResultSet result = connection.prepareStatement(query).executeQuery()) {
-            return DAOFiller.fillFromResultSet(type, result);
+        ParameterizedQuery parameterizedQuery = ParameterizedQuery.create(query);
+        parameterizedQuery.setParameters(parameters);
+
+        try (PreparedStatement statement = connection.prepareStatement(parameterizedQuery.getQuery())) {
+            parameterizedQuery.fill(statement);
+            try (ResultSet result = statement.executeQuery()) {
+                return DAOFiller.fillFromResultSet(type, result);
+            }
         }
+    }
+
+    public Persistence setParameter(String key, Object value) {
+        this.parameters.put(key, value);
+        return this;
     }
 }
