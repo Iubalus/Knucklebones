7a34bfd49c132c6ecde02f131d54dbde37931216
william.lowery@rocky.edu <william.lowery@rocky.edu>
2018-11-01 20:46:39 -0600

    Cleanup tests. Moved the sql that sets up test data and schema into its own directory. Modified WithInMemoryDB to initialize the db with the data and schema. This should provide a uniform set of data to test against. WWL

--- src/test/java/com/jubalrife/knucklebones/PersistenceTest.java
+++ src/test/java/com/jubalrife/knucklebones/PersistenceTest.java
@@ -10,13 +10,10 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class PersistenceTest extends WithInMemoryDB {
+
     @Test
     public void canFillDAOWithSimpleValues() throws Exception {
-        getConnection().prepareStatement("CREATE TABLE TableA ( ColumnA INT, ColumnB VARCHAR(50) )").executeUpdate();
-        getConnection()
-                .prepareStatement("INSERT INTO TableA (ColumnA, ColumnB) VALUES(1, 'Hello World')")
-                .executeUpdate();
-        List<TableADAO> found = new Persistence(getConnection()).find("SELECT * FROM TableA", TableADAO.class);
+        List<TableADAO> found = new Persistence(getConnection()).find("SELECT * FROM TableA WHERE ColumnA = 1", TableADAO.class);
         assertThat(found.size(), is(1));
         assertThat(found.get(0).columnA, is(1));
         assertThat(found.get(0).columnB, is("Hello World"));
@@ -24,20 +21,12 @@ public class PersistenceTest extends WithInMemoryDB {
 
     @Test
     public void canFindWithSimpleParmetersDAOWithSimpleValues() throws Exception {
-        getConnection().prepareStatement("CREATE TABLE TableB ( ColumnA INT, ColumnB VARCHAR(50) )").executeUpdate();
-        getConnection()
-                .prepareStatement("INSERT INTO TableB (ColumnA, ColumnB) VALUES(1, 'Hello World')")
-                .executeUpdate();
-        getConnection()
-                .prepareStatement("INSERT INTO TableB (ColumnA, ColumnB) VALUES(2, 'Good Bye, Moon')")
-                .executeUpdate();
-
-        List<TableADAO> found = new Persistence(getConnection()).setParameter("a", 1).find("SELECT * FROM TableB WHERE ColumnA = :a ", TableADAO.class);
+        List<TableADAO> found = new Persistence(getConnection()).setParameter("a", 1).find("SELECT * FROM TableA WHERE ColumnA = :a ", TableADAO.class);
         assertThat(found.size(), is(1));
         assertThat(found.get(0).columnA, is(1));
         assertThat(found.get(0).columnB, is("Hello World"));
 
-        found = new Persistence(getConnection()).setParameter("a", "Good Bye, Moon").find("SELECT * FROM TableB WHERE ColumnB = :a ", TableADAO.class);
+        found = new Persistence(getConnection()).setParameter("a", "Good Bye, Moon").find("SELECT * FROM TableA WHERE ColumnB = :a ", TableADAO.class);
         assertThat(found.size(), is(1));
         assertThat(found.get(0).columnA, is(2));
         assertThat(found.get(0).columnB, is("Good Bye, Moon"));
---
+++ src/test/resources/com/jubalrife/knucklebones/sql/schema/TableA.sql
@@ -0,0 +1,1 @@
+CREATE TABLE TableA ( ColumnA INT, ColumnB VARCHAR(50) );
